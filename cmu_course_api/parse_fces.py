f377acc1289526aa2e6d230a0b5b9e9a7e8fc934
Justin Gallagher <justingallag@gmail.com>
2016-07-12 20:09:36 +0000

    Correct Course ID format, null fields, and categorized questions

--- cmu_course_api/parse_fces.py
+++ cmu_course_api/parse_fces.py
@@ -19,19 +19,45 @@ import csv
 def parse_fces(path):
 
     results = []
+    categories = []
 
     # Iterate through lines of CSV
-    categories = []
     with open(path, 'r') as f:
         for line in csv.reader(f):
+
+            # If this row specifies new column tags, update our categories
             if line[0] == 'Semester':
                 categories = line
-            else:
-                entry = {}
-                for cat in range(len(categories)):
-                    if categories[cat] != '':
-                        entry[categories[cat]] = line[cat]
-                results.append(entry)
+                continue
+
+            entry = {}
+            entry['Questions'] = {}
+
+            for cat in range(len(categories)):
+
+                # Set null if no data supplied
+                if line[cat] == '':
+                    line[cat] = None
+
+                # Skip unused columns
+                if categories[cat] == '':
+                    continue
+
+                # Ensure course IDs have the proper format (##-###)
+                if categories[cat] == 'Course ID' and line[cat] != None:
+                    num = line[cat]
+                    if len(num) == 4:
+                        num = '0' + num
+                    entry[categories[cat]] = num[:2] + '-' + num[2:]
+
+                # If a category starts with a number, file it as a question
+                elif categories[cat][0].isdigit():
+                    entry['Questions'][categories[cat]] = line[cat]
+
+                else:
+                    entry[categories[cat]] = line[cat]
+
+            results.append(entry)
 
     # Return as JSON
     return results
