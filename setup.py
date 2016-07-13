e5e8b1dd467350ce1a5e9a6c8e398fd19c828a61
Justin Gallagher <justingallag@gmail.com>
2016-07-13 17:49:45 +0000

    Increment version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup
 
 setup(name='cmu-course-api',
-      version='1.4.0',
+      version='1.5.0',
       description=('Python utility for retrieving information about courses at'
                    ' Carnegie Mellon University.'),
       url='http://scottylabs.org/course-api',
